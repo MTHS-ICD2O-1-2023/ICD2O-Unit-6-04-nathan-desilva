82478783f712b8d069ddf75b35d43b6b137690bc
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-24 18:18:36 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+
+  // process
+  const volume = ((4 / 3) * Math.PI) * radius ** 3
+
+  // output
+  document.getElementById('radius').innerHTML = "The radius is: " + radius + ' mm'
+  document.getElementById('volume').innerHTML = 'The volume is: ' + volume.toFixed(2) + ' mm³'
+}
